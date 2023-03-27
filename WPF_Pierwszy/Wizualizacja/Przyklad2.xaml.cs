cd453c00e800c4965972acc6f4b0ba5cba155f34
komputer <komputer@DESKTOP-I0OKD5H>
2023-03-27 21:31:43 +0200

    Dodaj pliki projektów.

---
+++ WPF_Pierwszy/Wizualizacja/Przyklad2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Pierwszy.Wizualizacja
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Przyklad2.xaml
+    /// </summary>
+    public partial class Przyklad2 : Window
+    {
+        public Przyklad2()
+        {
+            InitializeComponent();
+        }
+    }
+}
